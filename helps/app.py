cbc2d0ca230da1a786270281be10cbd878b1a61f
gulajmaciej <38733860+gulajmaciej@users.noreply.github.com>
2018-11-18 14:57:15 +0100

    Update app.py

--- helps/app.py
+++ helps/app.py
@@ -1,13 +1,19 @@
-from __future__ import print_function
-import time
-from flask import Flask, render_template, Response
-from camera_pi import Camera
-from pololu_drv8835_rpi import motors, MAX_SPEED
-
+import RPi.GPIO as GPIO
+from flask import Flask, render_template
+from flask import request
+ 
+GPIO.setmode(GPIO.BCM)
+ 
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+ 
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+ 
+ 
 app = Flask(__name__)
-buttonStatus = 1
-
-@app.route('/')
+ 
+@app.route("/")
 def index():
     return render_template('index.html')
 
@@ -16,31 +22,57 @@ def gen(camera):
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    while buttonStatus == 1:
-	test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
-      [MAX_SPEED] * 200 + list(range(MAX_SPEED, 0, -1)) + [0]
-
-    	try:
-            motors.setSpeeds(0, 0)
-            for s in test_forward_speeds:
-            	motors.motor1.setSpeed(s)
-                motors.motor2.setSpeed(s)
-            	time.sleep(0.005)
-    	finally:
-        	motors.setSpeeds(0, 0)
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/background_process_test')
-def background_process_test():
-    if buttonStatus == 1:
-	buttonStatus = 0
+ 
+@app.route("/left")
+def left():
+    method = request.args.get('method')
+    if method == 'stop':
+        sig = GPIO.LOW
     else:
-        buttonStatus = 1
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=3000, debug=True)
+        sig = GPIO.HIGH
+ 
+    GPIO.output(12, sig)
+    return "OK"
+ 
+@app.route("/forward")
+def forward():
+    method = request.args.get('method')
+    if method == 'stop':
+        sig = GPIO.LOW
+    else:
+        sig = GPIO.HIGH
+ 
+    GPIO.output(6, sig)
+    GPIO.output(13, sig)
+    return "OK"
+ 
+@app.route("/backward")
+def backward():
+    method = request.args.get('method')
+    if method == 'stop':
+        sig = GPIO.LOW
+    else:
+        sig = GPIO.HIGH
+ 
+    GPIO.output(5, sig)
+    GPIO.output(12, sig)
+    return "OK"
+ 
+@app.route("/right")
+def right():
+    method = request.args.get('method')
+    if method == 'stop':
+        sig = GPIO.LOW
+    else:
+        sig = GPIO.HIGH
+ 
+    GPIO.output(5, sig)
+    return "OK"
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=7000, debug=True)
