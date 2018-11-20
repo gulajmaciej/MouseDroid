eeb8320de641bd01d76ae56edf5f67b2cad260ed
gulajmaciej <38733860+gulajmaciej@users.noreply.github.com>
2018-11-20 12:08:16 +0100

    Update app.py

--- helps/app.py
+++ helps/app.py
@@ -17,10 +17,10 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-#@app.route('/video_feed')
-#def video_feed():
-#    return Response(gen(Camera()),
-#                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/forward')
 def forward():
