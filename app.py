f9956f25590a141a655ba5c050b5e0cabb297d7a
gulajmaciej <38733860+gulajmaciej@users.noreply.github.com>
2018-11-20 21:22:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ class Control(threading.Thread):
         speed = 360
         try:
             print("forward speed: " + str(speed))
-            speed += 1
             motors.motor1.setSpeed(speed)
             motors.motor2.setSpeed(speed)
             time.sleep(0.005)
@@ -42,9 +41,7 @@ class Control(threading.Thread):
     def left():
         speed = 360
         try:
-#        while speed < 480:
             print("left speed: " + str(speed))
-            speed += 1
             motors.motor1.setSpeed(speed)
             motors.motor2.setSpeed(speed / 2)
             time.sleep(0.005)
@@ -57,7 +54,6 @@ class Control(threading.Thread):
         speed = 360
         try:
             print("right speed: " + str(speed))
-            speed += 1
             motors.motor1.setSpeed(speed / 2)
             motors.motor2.setSpeed(speed)
             time.sleep(0.005)
@@ -67,10 +63,9 @@ class Control(threading.Thread):
 
     @app.route('/backward')
     def backward():
-        speed = 360
+        speed = -360
         try:
             print("back speed: " + str(speed))
-            speed -= 1
             motors.motor1.setSpeed(speed / 2)
             motors.motor2.setSpeed(speed / 2)
             time.sleep(0.005)
@@ -78,15 +73,24 @@ class Control(threading.Thread):
             motors.setSpeeds(0, 0)
         return ('OK')
 
-
+    @app.route('/stop')
+    def stop():
+        speed = 0
+        motors.setSpeeds(0, 0)
+        motors.motor1.setSpeed(speed)
+        motors.motor2.setSpeed(speed)
+        
+        Return('OK')
+        
 if __name__ == '__main__':
    
     thread1 = Control()
     thread2 = Control()
     thread3 = Control()
     thread4 = Control()
-    thread5 = Cam()
+    thread5 = Control()
     thread6 = Cam()
+    thread7 = Cam()
     
     thread1.start()
     thread2.start()
@@ -94,4 +98,5 @@ if __name__ == '__main__':
     thread4.start()
     thread5.start()
     thread6.start()
-    app.run(host='0.0.0.0', debug=True)
+    thread7.start()
+    app.run(host='0.0.0.0', debug=True, thread =True)
